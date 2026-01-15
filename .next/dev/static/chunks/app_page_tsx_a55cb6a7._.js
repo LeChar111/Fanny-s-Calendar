4252d56d5f4302d71e841d43f206ae297e9cbb1f
Charles <charles.viron@hotmail.com>
2026-01-15 13:38:28 +0100

    Update tsconfig.json: format library array, change JSX setting to 'react-jsx', and expand include paths for TypeScript files.

---
+++ .next/dev/static/chunks/app_page_tsx_a55cb6a7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_87a09021._.js",
+  "static/chunks/node_modules_38620fef._.js"
+],
+    source: "dynamic"
+});
